8be10e35d9c3a980b3afff6fed19a615509082e5
Sanu Khan <sanulgbello@gmail.com>
2022-02-15 12:36:35 +0400

    Deleted Unwanted test code

--- include/profile/scripts.php
+++ include/profile/scripts.php
@@ -201,31 +201,13 @@
     }
 
     function init_audio(path) {
-        // return false;
 //         let myAudio = document.getElementById("family_pronunciation");
 // 	myAudio.load(); // Added for IOS devices By Sanu Khan 
 //         myAudio.play();
         // fetch(myAudio.children[0].src)
 // 	    console.log(path,'path')
 	    
-	   
-    function playAudio(src) {
-        if (!_snd)
-            _snd = new Audio();
-        else
-            $(_snd).empty();
-
-        for (var i = 0; i < src.length; i++) {
-            var source = document.createElement('source');
-            source.type = src[i].type;
-            source.src = src[i].src;
-            _snd.appendChild(source);
-        }
-
-        _snd.load(); // Needed on safari / idevice
-        _snd.play();
-    };
-
+	  
         fetch(path)
             .then(r => r.blob())
             .then(function(blobData){
